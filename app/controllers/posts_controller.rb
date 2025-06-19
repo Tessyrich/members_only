e11f4ff5ec50b7bc4369575386e42640503744c3
Tessyrich <olyrich3@gmail.com>
2025-06-19 14:53:52 +0100

    update the app so users can see post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+
+    def index
+        @posts = Post.includes(:user).order(created_at: :desc)
+      end
+      def new
+        @post =Post.new
+      end
+      def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+          redirect_to posts_path, notice: "Post created!"
+        else
+          render :new
+        end
+      end
+    
+      private
+    
+      def post_params
+        params.require(:post).permit(:content)
+      end
+end
